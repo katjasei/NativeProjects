a623041cae1c37da31c88acfdc9a0f194c83e3d0
Katja Seikkinen <katjasei@metropolia.fi>
2019-08-29 13:42:02 +0300

    Fetching data with AJAX and sharing it with Context

--- components/List.js
+++ components/List.js
@@ -1,22 +1,37 @@
-import React from 'react';
+import React, {useContext, useEffect} from 'react';
+import {FlatList} from 'react-native';
 import PropTypes from 'prop-types';
-import {
-  FlatList,
-} from 'react-native';
 import ListItem from './ListItem';
+import {MediaContext} from '../contexts/MediaContext';
+
+const dataUrl = "https://raw.githubusercontent.com/mattpe/wbma/master/docs/assets/test.json"
 
 const List = (props) => {
-  console.log(props);
-  return (
+  const [media, setMedia] = useContext(MediaContext);
+
+  const getMedia = () => {
+    fetch(dataUrl)
+    .then((response) => {
+      return response.json();
+    })
+    .then((result) => {
+      console.log(result),
+      setMedia(result);
+    });
+  };
+
+  useEffect(() => getMedia(), []);
+
+  return(
     <FlatList
-      data={props.mediaArray}
+      data={media}
       renderItem={({item}) => <ListItem singleMedia={item} />}
-    />
+      />
   );
 };
 
 List.propTypes = {
   mediaArray: PropTypes.array,
-};
+ };
 
 export default List;
